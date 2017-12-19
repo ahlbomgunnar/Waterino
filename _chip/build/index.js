93df095f865bc85a7fd17478d41c556af384c68c
Gunnar Ahlbom <Gahlbom@gmail.com>
2017-12-19 10:13:23 +0100

    Delete index.js

--- _chip/build/index.js
+++
@@ -1,8 +0,0 @@
-
-// CHIP_Watering_Handler by Niklas Nauber and Gunnar Ahlbom
-// All rights reserved 11/09-17
-
-var CHIP_CONTROLLER = require('./src/CHIP_CONTROLLER.js'); 
-
-var Waterino = new CHIP_CONTROLLER();
-    Waterino.start();
