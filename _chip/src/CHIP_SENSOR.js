e14e12a8b3a55989185e7a84b88adac41fdc81be
Gunnar Ahlbom <Gahlbom@gmail.com>
2017-12-19 12:05:13 +0100

    sensor read 750ms

--- _chip/src/CHIP_SENSOR.js
+++ _chip/src/CHIP_SENSOR.js
@@ -11,13 +11,11 @@ function CHIP_SENSOR(position, cmd) {
 
 	this.read = (callback) => {
 		cmd.run('echo "s' + this.position + ':read" > /dev/ttyACM0');
-		setTimeout(() => {
-			cmd.get('head -1 /dev/ttyACM0', (err, data, srderr) => {
-				if(!err && data != null && data != undefined) return callback(null, data);
-				else if(err) return callback(err);
-				else return callback(srderr);
-			});
-		}, 2000);
+		cmd.get('head -1 /dev/ttyACM0', (err, data, srderr) => {
+			if(!err && data != null && data != undefined) return callback(null, data);
+			else if(err) return callback(err);
+			else return callback(srderr);
+		}, 750);
 	};
 };
 
